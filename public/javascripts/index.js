c8bdb1bbcd86c591cbcdd9458f4b3ab37deb8bb0
Sebastian Olaizola <sebaolaizola@gmail.com>
2012-07-07 17:29:29 -0300

    Bars style and logic.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,65 @@
+var tuiters = tuiters || {};
+
+tuiters.fight = (function() {
+	var fightResult = {};
+	var total = 0
+
+	return {
+		getFightPercentage: function () {
+			total++;
+			$.each(counters, function(e){
+				fightResult[e] = this*100/total;
+			});
+			return fightResult;
+		},
+		reset: function () {
+			fightResult = {};
+			total = 0; 
+		}
+	};
+}) ();
+
+tuiters.render = (function () {
+	return {
+		horizontalBars: function() {
+			$.each(tuiters.fight.getFightPercentage(), function(e){
+			var div = $('#' + e);
+
+				if (!div.length) {
+					div = $('<div id=' + e + '>');
+					div.css('background-color',  getRandomColor());
+					div.addClass('horizontalBar');
+					div.appendTo('#bars');
+				}
+
+				div.text(e + ': ' + redondear(this, 2)) 
+				div.css('width', this + '%');
+			})
+		},
+		verticalBars: function() {
+			$.each(tuiters.fight.getFightPercentage(), function(e){
+
+				var div = $('#' + e);
+
+				if (!div.length) {
+					div = $('<div id=' + e + '>');
+					div.css('background-color',  getRandomColor());
+					div.addClass('verticalBar');
+
+					div.appendTo($('<div id="contentBar' + e + '">').addClass('contentBar').appendTo('#bars'));
+				}
+				div.empty();
+				var span = $('<span>');
+				span.css('position', 'absolute');
+				span.css('bottom', '10px');
+				span.addClass('resultBarText');
+				//span.text(e + ': ' + redondear(this, 2)).appendTo(div); 
+				span.text(e).appendTo(div); 
+				div.css('height', this + '%');
+			})
+		}
+	};
+}) ();
 
 var socket = io.connect('http://localhost'),
 	counters = {};
@@ -7,7 +69,6 @@ socket.on('tweet', function (q) {
 		window.counters[q.keyword] = 0;
 	window.counters[q.keyword] ++;
 	rebind();
-	console.log(q);
 });
 
 $(document).on('ready', function(){
@@ -16,6 +77,8 @@ $(document).on('ready', function(){
 
 	$('#go').on('click', function(){
 		$('#tweets').empty();
+		$('#bars').empty();
+		tuiters.fight.reset();
 		window.counters = {};
 		socket.emit('newSearch', $('#keywords').val().split(','));
 	});
@@ -26,4 +89,14 @@ function rebind(){
 	$.each(counters, function(e){
 		$('<p>').text(e + ' : ' + this).appendTo('#tweets');
 	});
+
+	//tuiters.render.horizontalBars();
+	tuiters.render.verticalBars();
+}
+
+function redondear(cantidad, decimales) {
+	return Math.round(cantidad * Math.pow(10, decimales)) / Math.pow(10, decimales);
+} 
+function getRandomColor() {
+	return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 }
